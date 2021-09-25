dce246f757b8a13cbecd199ff5a4c5ffd5c09f51
Li Xian <59865111+lixiannn@users.noreply.github.com>
2021-09-25 23:37:44 +0800

    update project settings & added start page

---
+++ StartPage.gd
@@ -0,0 +1,26 @@
+extends CanvasLayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_RegisterButton_pressed():
+	pass # Replace with function body.
+	# navigate to page where they can register as teacher or student
+
+
+func _on_LoginButton_pressed():
+	pass # Replace with function body.
+	# navigate to page where they can login as teacher or student
