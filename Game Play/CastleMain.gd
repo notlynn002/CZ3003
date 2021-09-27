3e0d287e46437f3f6f8591c234d79a1b89013f70
yinjiarui27 <62418592+yinjiarui27@users.noreply.github.com>
2021-09-27 22:51:57 +0800

    normal level updated

--- Game Play/CastleMain.gd
+++ Game Play/CastleMain.gd
@@ -8,10 +8,13 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$PopupMenu.hide() # dont showpopup until one of the castle is pressed
 
 func _on_Qn5_pressed():
 	get_tree().change_scene("res://Game Play/NormalLevelQn.tscn")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func _on_Qn1_pressed():
+	$PopupMenu.show() # display popup
