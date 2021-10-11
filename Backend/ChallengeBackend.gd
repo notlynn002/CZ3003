a53b2670e23b4870e84f41468c0308cae90f640e
bloppers <wencheng1298@gmail.com>
2021-10-11 10:32:23 +0800

    Reject challenge backend function

--- Backend/ChallengeBackend.gd
+++ Backend/ChallengeBackend.gd
@@ -5,6 +5,7 @@ extends CanvasLayer
 func _ready():
 	pass # Replace with function body.
 
+# ------------ Get Random questions and Create Challenge --------------------
 
 func get_towerid_by_topic(topic):
 	var query : FirestoreQuery = FirestoreQuery.new()
@@ -32,7 +33,6 @@ func get_levelIds_of_tower(towerId):
 func get_qns_from_levelIds(levelIds):
 	var query : FirestoreQuery = FirestoreQuery.new()
 	query.from('Question')
-	#Operator in not working?
 	query.where('levelID', FirestoreQuery.OPERATOR.IN, levelIds)
 	
 	var query_task : FirestoreTask = Firebase.Firestore.query(query)
@@ -62,7 +62,6 @@ func getRandomQuestionId(topic):
 		randomed_qn_ids.append(questionIds[i])
 	return randomed_qn_ids
 
-
 #Pass in topic name
 func _on_Get_10_Random_Qns_button_up():
 	var topic = 'Four Operations' # Example topic name
@@ -85,7 +84,6 @@ func createChallenge(challengeQns, challenger_time, challenger_score, challenger
 	
 	task = collection.add("", challengeDetails)
 	yield(task, "task_finished")
-	print('Added Challenge')
 
 #
 func _on_Create_Challenge_button_up():
@@ -99,3 +97,29 @@ func _on_Create_Challenge_button_up():
 	
 	# Calling of function
 	yield(createChallenge(challenge_questions, challenger_time, challenger_score, challenger_id, challengee_id), 'completed')
+
+
+#------------ Reject Challenge----------------
+func reject_challenge(challengeId, challengeeid):
+	var collection : FirestoreCollection = Firebase.Firestore.collection('Challenge')
+	collection.get(challengeId)	
+	var challenge :FirestoreDocument = yield(collection, "get_document")
+	
+	# Get all challengeeIds for challenge and remove the denoted challengeeid
+	var challengeeId = challenge.doc_fields.challengeeID
+	
+	var updated_challengeeId = []
+	for id in challengeeId:
+		if id != challengeeid:
+			updated_challengeeId.append(id)
+	
+	#Update to firestore
+	var task: FirestoreTask
+	task = collection.update(challengeId, {'challengeeID': updated_challengeeId})
+	yield(task, "task_finished")
+	
+
+func _on_Reject_Challenge_button_up():
+	var challengeId = 'NqHewZ5mStJaco9dECqT'
+	var challengeeId = 'P8zkYTNczGZcwLMnkbQBJsscBNp1'
+	reject_challenge(challengeId, challengeeId)
