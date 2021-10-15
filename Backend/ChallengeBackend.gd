7335277374490fa2eaf09e114e4021ff0745cdcf
bloppers <wencheng1298@gmail.com>
2021-10-16 00:34:17 +0800

    Updated Challenge Functions

--- Backend/ChallengeBackend.gd
+++ Backend/ChallengeBackend.gd
@@ -14,6 +14,7 @@ func get_towerid_by_topic(topic):
 
 	var query_task : FirestoreTask = Firebase.Firestore.query(query)
 	var towerId = yield(query_task, 'task_finished')
+	#print(towerId)
 	var result = towerId[0].doc_name
 	return result
 	
@@ -64,17 +65,18 @@ func getRandomQuestionId(topic):
 
 #Pass in topic name
 func _on_Get_10_Random_Qns_button_up():
-	var topic = 'Four Operations' # Example topic name
+	var topic = 'numbers' # Example topic name
 	
 	var challenge_questions = yield(getRandomQuestionId(topic), 'completed')
 	print(challenge_questions)
 
 
-func createChallenge(challengeQns, challenger_time, challenger_score, challenger_id, challengee_id):
+# Initial creation of challenge. Takes in the challenge topic, challenger_id and challengee_id
+func createChallenge(topic, challenger_id, challengee_id):
+	var challenge_questions = yield(getRandomQuestionId(topic), 'completed')
+	
 	var challengeDetails = {
-		'questionList' : challengeQns,
-		'challengerTime' : challenger_time,
-		'challengerScore' : challenger_score,
+		'questionList' : challenge_questions,
 		'challengerID' : challenger_id,
 		'challengeeID' : challengee_id
 	}
@@ -83,20 +85,57 @@ func createChallenge(challengeQns, challenger_time, challenger_score, challenger
 	var collection : FirestoreCollection = Firebase.Firestore.collection('Challenge')
 	
 	task = collection.add("", challengeDetails)
-	yield(task, "task_finished")
+	var challengeID = yield(task, "task_finished")
+	
+	# Create Challengee Records for each of the challengees
+	
+	
+	return challengeID.doc_name
 
-#
 func _on_Create_Challenge_button_up():
 	# Example inputs
-	var topic = 'Four Operations' # Example topic name
-	var challenge_questions = yield(getRandomQuestionId(topic), 'completed') # Example questions
-	var challenger_time = 5
-	var challenger_score = 5
+	var topic = 'numbers' 
 	var challenger_id = 'XKwVQ9EqJ7xjEhHoPr0A'
 	var challengee_id = ['P8zkYTNczGZcwLMnkbQBJsscBNp1']
 	
 	# Calling of function
-	yield(createChallenge(challenge_questions, challenger_time, challenger_score, challenger_id, challengee_id), 'completed')
+	yield(createChallenge(topic, challenger_id, challengee_id), 'completed')
+
+
+# Updates the result for a challenge. 
+func updateChallengeResult(challengeId, score, time, role, userId):
+	var task: FirestoreTask
+	var collection : FirestoreCollection;
+	# Update record for challenger
+	if role == 'challenger':
+		collection = Firebase.Firestore.collection('Challenge')
+		task = collection.update(challengeId, {'challengerScore': score, 'challengertime' : time})
+	# Update record for challengee
+	else:
+		# Search challengee record using challengeID - Obtain the challengee_record id.
+		var query : FirestoreQuery = FirestoreQuery.new()
+		query.from('Challengee_Record')
+		query.where('challengeID', FirestoreQuery.OPERATOR.EQUAL, challengeId)
+		query.where('challengeeID', FirestoreQuery.OPERATOR.EQUAL, userId)
+	
+		var query_task : FirestoreTask = Firebase.Firestore.query(query)
+		var challengeeRecord = yield(query_task, 'task_finished')
+		#get the recordId out
+		var recordId = challengeeRecord[0].doc_name
+		# Update the challengee record
+		collection = Firebase.Firestore.collection('Challengee_Record')
+		task = collection.update(recordId, {'challengeeScore': score, 'challengeetime' : time, 'challengeStatus': 'completed'})
+		
+	yield(task, 'task_finished')
+	print('Record Updated!')
+
+func _on_Update_Challenge_Result_button_up():
+	var challengeId = ' KdsBS2748cPpgyxkP532'
+	var challengeeId = 'P8zkYTNczGZcwLMnkbQBJsscBNp1'
+	var score = 2
+	var time = 1
+	var role = 'challengee'
+	updateChallengeResult(challengeId, score, time, role, challengeeId)
 
 
 #------------ Reject Challenge----------------
@@ -123,3 +162,5 @@ func _on_Reject_Challenge_button_up():
 	var challengeId = 'NqHewZ5mStJaco9dECqT'
 	var challengeeId = 'P8zkYTNczGZcwLMnkbQBJsscBNp1'
 	reject_challenge(challengeId, challengeeId)
+	
+	
