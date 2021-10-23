b94e5867b331c31ed1c882848391cd6cd8fd3d2a
cheyanniee <60493204+cheyanniee@users.noreply.github.com>
2021-10-23 16:51:47 +0800

    Add files via upload

---
+++ Backend/SocialMediaBackend.gd
@@ -0,0 +1,74 @@
+extends Control
+
+class_name SocialMediaBackend
+
+onready var http= $HTTPRequest
+
+var fb_app_id = "408168340818896"
+var fb_page_id = ""
+var fb_login_dict = {
+	"app_id": fb_app_id,
+	"http": "https://www.facebook.com/v12.0/dialog/oauth?display=popup&",
+	"redirect_uri": "https://localhost:6007/"
+}
+var fb_share_dict = {
+	"app_id": fb_app_id,
+	"http": "https://graph.facebook.com/" + fb_page_id + "/feed?",
+	"redirect_uri": ""
+}
+
+
+func _ready():
+	Firebase.Auth.login_with_email_and_password("admin@gmail.com", "cz3003ssad")
+	
+
+func fb_login():
+	OS.shell_open(fb_login_dict["http"] + 
+		"client_id=" + fb_login_dict["app_id"] + 
+		"&redirect_uri=" + fb_login_dict["redirect_uri"])
+	
+	var response = yield(http, "request_completed")
+	print(response)
+	print("yayy")
+	
+	return response
+
+
+func fb_post(access_token, message, link):
+	message = message.replace(" ", "%20")
+	
+	http.request(fb_share_dict["http"] + 
+		"access_token=" + access_token + 
+		"&message=" + message + 
+		"&link=" + link)
+	
+	var response = yield(http, "request_completed")
+	print(response)
+	
+	return response
+
+
+func twitter_login():
+	pass
+
+
+func twitter_post(message, link):
+	pass
+
+
+func _on_fb_login_button_up():
+	print("testing FB Login")
+	fb_login()
+
+
+func _on_fb_post_button_up():
+	print("testing FB Post")
+	fb_post("access_token", "Test Post", "google.com")
+
+
+func _on_twitter_login_button_up():
+	print("testing Twitter Login")
+
+
+func _on_twitter_post_button_up():
+	print("testing Twitter Post")
